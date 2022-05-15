9cf1d896d74cb0c73018dc0c2584c41a4ec37a42
aizjicod <98921366+aizjicod@users.noreply.github.com>
2022-05-14 22:46:41 -0400

    finished first part of the first chap

---
+++ chap 7 js/window.js
@@ -0,0 +1,23 @@
+let ventana;
+// to open a new windows
+// ventana = window.open("https://www.youtube.com/watch?v=xOinGb2MZSk");
+
+// to close a windows
+// ventana.close();
+// //to verify if a window is closed
+// ventana.closed;
+
+// this method stops the downlowd of resources in a page
+// ventana.stop();
+
+//with alert we can only use the keyword "alert", it is not necesary to use the window.alert();
+// alert("bruja");
+
+//print the actual document
+// print();
+
+//getting data
+// prompt("this data is a string");
+
+// to confirm an action this return true/false;
+confirm("you sure want to exit the page?");
